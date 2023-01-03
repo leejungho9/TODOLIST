b8fefa89f90b850e7b662a76258f25a6e6a6c966
18137036 <9wjdgh9@naver.com>
2023-01-03 22:55:43 +0900

    Feat : todoList 추가 기능 구현

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useTodoState } from '../Context/TodoContext';
 import TodoItem from './TodoItem';
 
 
@@ -8,16 +9,16 @@ const TodoListBlock = styled.div`
   padding: 20px 32px;
   padding-bottom: 48px;
   overflow-y: auto;
-  /* background: gray; 사이즈 조정이 잘 되고 있는지 확인하기 위한 임시 스타일 */
 `;
 
 function TodoList(props) {
+    const todos = useTodoState();
+
     return (
        <TodoListBlock>
-        <TodoItem text="프로젝트 생성하기" done={true}></TodoItem>
-        <TodoItem text="컴포넌트 스타일링 하기" done={true}></TodoItem>
-        <TodoItem text="Context 만들기" done={false}></TodoItem>
-        <TodoItem text="기능 구현하기" done={false}></TodoItem>
+        {todos.map(todo => (
+            <TodoItem key={todo.id} id={todo.id} text={todo.text} done={todo.done}/>
+        ))}
        </TodoListBlock>
     );
 }
